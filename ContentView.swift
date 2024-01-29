1f5b8f0e84b02840a3cd236550fb9a5b471bb703
Programming God <storytellor@gmail.com>
2024-01-30 05:14:42 +0900

    Dutch pay app

--- ContentView.swift
+++ ContentView.swift
@@ -1,30 +1,74 @@
 //
 //  ContentView.swift
-//  WeSplit
+//  DutchPayApp
 //
-//  Created by Bumsu Kim on 1/28/24.
+//  Created by Bumsu Kim on 1/30/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Harry", "Hermeone", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var totalAmount : Double = 0
+    @State private var tipPercentage : Int = 20
+    @State private var numberOfPeople : Int = 2
+    
+    @FocusState private var totalAmountFocused : Bool
+    
+    let tipPercentages = [10,15, 20,25,0]
+    
+    var perPerson : Double {
+        
+        return (Double(totalAmount) + Double(totalAmount) * (Double(tipPercentage)/100)) / Double(numberOfPeople)
+    }
     
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent){
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                
+                Section {
+                    TextField("Total Amount", value: $totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                        .focused($totalAmountFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        
+                        ForEach(2..<100) {
+                            Text("\($0-2)")
+                        }
+                    }
+                    
+                    
+                }
+                
+                Section("Tip Percentage") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section("Dutch pay price") {
+                    
+                    Text(perPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }.navigationTitle("Dutch Pay App")
+                .toolbar {
+                    if totalAmountFocused {
+                        Button("Done") {
+                            totalAmountFocused = false
+                        }
+                        
                     }
                 }
-            }
-        }.navigationTitle("Select a student")
+        }
+        
+        
     }
+    
+    
 }
+
 #Preview {
     ContentView()
 }
-
---
+++ DutchPayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DutchPayAppApp.swift
+//  DutchPayApp
+//
+//  Created by Bumsu Kim on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DutchPayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
